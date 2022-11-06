6ba7cfb8569c037f1a5d3fa4cbb7aa30bc0bb240
adelekedare <adelekedare15@gmail.com>
2022-11-06 03:55:25 -0800

    'Argument count and vector'

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - Prints the program name, followed by a new line.
+ * @argc: The nuber of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always 0 .
+ */
+
+int main(argc, char *argv[])
+
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
